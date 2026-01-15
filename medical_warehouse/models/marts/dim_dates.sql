7f46cb28746b3c57172a3ed946e4605fc427bf73
gashawbekele06git config --global user.email gashawbeklek@gmail.com <gashawbekelek@gmail.com>
2026-01-15 21:07:39 +0300

    dim date created

---
+++ medical_warehouse/models/marts/dim_dates.sql
@@ -0,0 +1,23 @@
+{{
+  config(materialized = 'table', schema = 'marts')
+}}
+
+WITH dates AS (
+  SELECT generate_series(
+    '2024-01-01'::date,
+    CURRENT_DATE + INTERVAL '1 year',
+    '1 day'
+  )::date AS full_date
+)
+SELECT
+  TO_CHAR(full_date, 'YYYYMMDD')::int AS date_key,
+  full_date,
+  EXTRACT(DOW FROM full_date) AS day_of_week,
+  TO_CHAR(full_date, 'Day') AS day_name,
+  EXTRACT(WEEK FROM full_date) AS week_of_year,
+  EXTRACT(MONTH FROM full_date) AS month,
+  TO_CHAR(full_date, 'Month') AS month_name,
+  EXTRACT(QUARTER FROM full_date) AS quarter,
+  EXTRACT(YEAR FROM full_date) AS year,
+  CASE WHEN EXTRACT(DOW FROM full_date) IN (0,6) THEN TRUE ELSE FALSE END AS is_weekend
+FROM dates
