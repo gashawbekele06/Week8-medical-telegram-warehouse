c606ef2dd14bf6d7584cdc332ae2380c601d2755
gashawbekele06git config --global user.email gashawbeklek@gmail.com <gashawbekelek@gmail.com>
2026-01-15 21:09:39 +0300

    commit

---
+++ medical_warehouse/models/staging/stg_telegram_messages.sql
@@ -0,0 +1,30 @@
+{{
+  config(
+    materialized = 'view',
+    schema = 'staging'
+  )
+}}
+
+WITH source AS (
+  SELECT *
+  FROM {{ source('raw', 'telegram_messages') }}
+)
+
+SELECT
+  message_id,
+  channel_username,
+  channel_title,
+  date::timestamptz               AS message_timestamp,
+  text                            AS message_text,           -- rename here (optional but good)
+  views                           AS view_count,
+  forwards                        AS forward_count,
+  has_media,
+  image_path,
+  LENGTH(text)                    AS message_length,
+  CASE WHEN image_path IS NOT NULL THEN TRUE ELSE FALSE END AS has_image,
+  DATE(date::timestamptz)         AS message_date
+FROM source
+WHERE 
+  text IS NOT NULL                   -- ← use the ORIGINAL column name from raw table
+  AND text != ''
+  AND date IS NOT NULL
---
+++ tests/assert_no_future_messages.sql
@@ -0,0 +1,4 @@
+SELECT *
+FROM {{ ref('fct_messages') }} f
+JOIN {{ ref('dim_dates') }} d ON f.date_key = d.date_key
+WHERE d.full_date > CURRENT_DATE
